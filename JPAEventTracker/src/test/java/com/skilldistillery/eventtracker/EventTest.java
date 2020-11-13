4776bd84ca73c53f3d585ed45aea08626312913a
Daniel Majewski <d.majewski.3453@gmail.com>
2020-11-13 16:11:26 -0700

    good entity test

--- JPAEventTracker/src/test/java/com/skilldistillery/eventtracker/EventTest.java
+++ JPAEventTracker/src/test/java/com/skilldistillery/eventtracker/EventTest.java
@@ -1,17 +1,28 @@
 package com.skilldistillery.eventtracker;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class EventTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Event event;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("EventPU");
 	}
 
 	@AfterAll
@@ -20,15 +31,21 @@ class EventTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		event  = em.find(Event.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		event = null;
 	}
 
 	@Test
+	@DisplayName("test Event entity")
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(event);
+		assertEquals("event name", event.getName());
 	}
 
 }
