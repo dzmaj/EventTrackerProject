57eea8cd6f34cc0fef0351c88c047f6cbf770c5f
Daniel Majewski <d.majewski.3453@gmail.com>
2020-11-15 21:42:33 -0700

    job controller

--- EventTracker/src/main/java/com/skilldistillery/eventtracker/controllers/JobController.java
+++ EventTracker/src/main/java/com/skilldistillery/eventtracker/controllers/JobController.java
@@ -6,8 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +43,63 @@ public class JobController {
 			response.setStatus(404);
 		}
 		return job;
+	}
+	
+	@PostMapping("jobs")
+	public Job create(@RequestBody Job job, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			job = jobSvc.create(job);
+			response.setStatus(201);
+			StringBuffer url = request.getRequestURL();
+			url.append("/").append(job.getId());
+			response.setHeader("Location", url.toString());
+		} catch (Exception e) {
+			response.setStatus(400);
+			job = null;
+		}
+		
+		return job;
+	}
+	
+	@DeleteMapping("jobs/{id}")
+	public void delete(@PathVariable Integer id, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			Boolean deleted = jobSvc.delete(id);
+			if (deleted == null) {
+				response.setStatus(404);
+			} else if (!deleted) {
+				response.setStatus(400);
+			} else {
+				response.setStatus(204);
+			}
+		} catch (Exception e) {
+			response.setStatus(400);
+		}
+	}
+	
+	@PutMapping("jobs/{id}")
+	public Job update(@RequestBody Job job, @PathVariable Integer id,
+						HttpServletRequest request, HttpServletResponse response) {
+		try {
+			job = jobSvc.update(job, id);
+			if (job == null) {
+				response.setStatus(404);
+			} else {
+				response.setStatus(202);
+				StringBuffer url = request.getRequestURL();
+				response.setHeader("Location", url.toString());
+			}
+		} catch (Exception e) {
+			response.setStatus(400);
+			job = null;
+		}
+		
+		return job;
+	}
+	
+	@GetMapping("jobs/search/{keyword}")
+	public List<Job> searchKeyword(@PathVariable String keyword, HttpServletRequest request, HttpServletResponse response) {
+		return jobSvc.findByKeyword(keyword);
 	}
 
 }
