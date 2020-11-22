d6c140ad393871c6069fd8a760d1bdf363f37f22
Daniel Majewski <d.majewski.3453@gmail.com>
2020-11-21 23:41:18 -0700

    detail view, update, delete

--- EventTracker/src/main/resources/static/js/jobTracker.js
+++ EventTracker/src/main/resources/static/js/jobTracker.js
@@ -1,10 +1,9 @@
 window.addEventListener("load", function () {
-  console.log("Script loaded");
   init();
 });
 
 function init() {
-  console.log("in init()");
+  document.getElementById('detailView').style.display = 'none';
   //TODO: Set up events
   getAllJobs();
   document.createForm.submit.addEventListener("click", function (e) {
@@ -12,14 +11,14 @@ function init() {
     let form = e.target.parentElement;
     let newJob = {
       position: form.position.value,
-      description: form.description.value,
+      description: document.getElementById('createDescription').value,
       company: form.company.value,
       city: form.city.value,
       state: form.state.value,
       source: form.source.value,
       url: form.url.value,
     };
-    addJob(newJob);
+	addJob(newJob);
   });
 }
 
@@ -58,8 +57,9 @@ function addJobToTable(job) {
 
 function buildJobsTable(jobs) {
   let tableDiv = document.getElementById("tableDiv");
+  tableDiv.textContent = "";
   let table = document.createElement("table");
-  table.setAttribute("class", "table");
+  table.setAttribute("class", "table table-bordered");
   table.setAttribute("id", "jobs-table");
   tableDiv.appendChild(table);
   
@@ -78,8 +78,11 @@ function buildJobsTable(jobs) {
   th3.textContent = "Description";
   headRow.appendChild(th3);
   let th4 = document.createElement("th");
-  th4.textContent = "Location";
+  th4.textContent = "City";
   headRow.appendChild(th4);
+  let th4b = document.createElement("th");
+  th4b.textContent = "State";
+  headRow.appendChild(th4b);
   let th5 = document.createElement("th");
   th5.textContent = "Status";
   headRow.appendChild(th5);
@@ -94,9 +97,63 @@ function buildJobsTable(jobs) {
   let body = document.createElement("tbody");
   body.setAttribute("id", "jobs-table-body");
   table.appendChild(body);
-  jobs.forEach((element) => {
-    let row = document.createElement("tr");
-    body.appendChild(row);
+  jobs.forEach((element, index) => {
+	let row = document.createElement("tr");
+	row.setAttribute('id', index)
+	body.appendChild(row);
+	row.addEventListener("click", function(e) {
+		e.preventDefault();
+		// let row = e.target;
+		// console.log(index);
+		// let job = jobs[index];
+		document.getElementById('detailView').style.display = 'initial';
+		document.updateForm.position.setAttribute('value', element.position);
+		document.updateForm.company.setAttribute('value', element.company);
+		document.getElementById('updateDescription').value = element.description;
+		document.updateForm.city.setAttribute('value', element.city);
+		document.updateForm.state.setAttribute('value', element.state);
+		// document.updateForm.status.setAttribute('value', 'status');
+		document.updateForm.source.setAttribute('value', element.source);
+		document.updateForm.url.setAttribute('value', element.url);
+		// document.updateForm.update.removeEventListener('click', function(e){});
+		// document.updateForm.delete.removeEventListener('click', function(e){});
+		if (document.updateForm.update !== undefined) {
+			document.updateForm.update.remove();
+		}
+		if (document.updateForm.deleteBtn !== undefined) {
+			document.updateForm.deleteBtn.remove();
+		}
+		let update = document.createElement('input');
+		let deleteBtn = document.createElement('input');
+		update.setAttribute('value', 'Update');
+		update.setAttribute('type', 'submit');
+		update.setAttribute('name', 'update');
+		deleteBtn.setAttribute('value', 'Delete');
+		deleteBtn.setAttribute('type', 'submit');
+		deleteBtn.setAttribute('name', 'deleteBtn');
+		document.updateForm.appendChild(update);
+		document.updateForm.appendChild(deleteBtn);
+		update.addEventListener('click', function(e) {
+			e.preventDefault();
+			console.log(document.getElementById('updateDescription'))
+			let updatedJob = {
+				id: element.id,
+				position: document.updateForm.position.value,
+				description: document.getElementById('updateDescription').value,
+				company: document.updateForm.company.value,
+				city: document.updateForm.city.value,
+				state: document.updateForm.state.value,
+				source: document.updateForm.source.value,
+				url: document.updateForm.url.value,
+			  };
+			updateJob(updatedJob);
+		});
+		deleteBtn.addEventListener('click', function(e) {
+			e.preventDefault();
+			deleteJob(element);
+		});
+		
+	});
 
     let data = document.createElement("td");
     data.textContent = element.position;
@@ -104,11 +161,18 @@ function buildJobsTable(jobs) {
     data = document.createElement("td");
     data.textContent = element.company;
     row.appendChild(data);
-    data = document.createElement("td");
-    data.textContent = element.description.slice(0, 30) + "...";
+	data = document.createElement("td");
+	if (element.description.length > 50) {
+		data.textContent = element.description.slice(0, 50) + "...";
+	} else {
+		data.textContent = element.description;
+	}
     row.appendChild(data);
     data = document.createElement("td");
-    data.textContent = element.city + ", " + element.state;
+    data.textContent = element.city;
+    row.appendChild(data);
+    data = document.createElement("td");
+    data.textContent = element.state;
     row.appendChild(data);
     data = document.createElement("td");
     data.textContent = "status";
@@ -119,8 +183,12 @@ function buildJobsTable(jobs) {
     data = document.createElement("td");
     if (typeof element.url === "string") {
       let url = document.createElement("a");
-      url.setAttribute("href", element.url);
-      url.textContent = element.url.slice(0, 20) + "...";
+	  url.setAttribute("href", element.url);
+	  if (element.url.length > 20) {
+		  url.textContent = element.url.slice(0, 20) + "...";
+	  } else {
+		  url.textContent = element.url;
+	  }
       data.appendChild(url);
     }
     row.appendChild(data);
@@ -135,7 +203,8 @@ function getAllJobs() {
       if (xhr.status === 200) {
         let jobs = JSON.parse(xhr.responseText);
         // console.log(jobs);
-        buildJobsTable(jobs);
+		buildJobsTable(jobs);
+		// jobsData = jobs;
       } else {
         console.error("Error unexpected response status " + xhr.status);
       }
@@ -152,7 +221,8 @@ function addJob(job) {
     if (xhr.readyState === 4) {
       if (xhr.status === 201) {
         let newJob = JSON.parse(xhr.responseText);
-        addJobToTable(newJob);
+		// addJobToTable(newJob);
+		getAllJobs();
       } else {
         console.error(
           "Error unexpected response status " +
@@ -165,4 +235,50 @@ function addJob(job) {
   };
 
   xhr.send(JSON.stringify(job));
+}
+
+function updateJob(job) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("PUT", "api/jobs/"+job.id, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function () {
+	  if (xhr.readyState === 4) {
+      if (xhr.status === 202) {
+		let updatedJob = JSON.parse(xhr.responseText);
+		getAllJobs();
+      } else {
+        console.error(
+          "Error unexpected response status " +
+            xhr.status +
+            ": " +
+            xhr.responseText
+        );
+      }
+    }
+  };
+
+  xhr.send(JSON.stringify(job));
+}
+
+function deleteJob(job) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("DELETE", "api/jobs/"+job.id, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 204) {
+		// addJobToTable(newJob);
+		getAllJobs();
+      } else {
+        console.error(
+          "Error unexpected response status " +
+            xhr.status +
+            ": " +
+            xhr.responseText
+        );
+      }
+	}
+  };
+
+  xhr.send();
 }
