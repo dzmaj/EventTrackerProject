d721fb42e695173a60fe387557dafa77279d7486
Daniel Majewski <d.majewski.3453@gmail.com>
2020-11-30 19:17:45 -0700

    stub angular job component, model, service

--- ngJobTracker/src/app/app.module.ts
+++ ngJobTracker/src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { JobService } from './services/job.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    JobService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
