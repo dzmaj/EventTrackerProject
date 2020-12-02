d4af68c02e4ace6f49ad653a104e64c36862fda9
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-01 17:30:17 -0700

    formatting

--- ngJobTracker/src/app/components/job-list/job-list.component.ts
+++ ngJobTracker/src/app/components/job-list/job-list.component.ts
@@ -14,6 +14,14 @@ export class JobListComponent implements OnInit {
   newJob: Job = new Job();
   selected = null;
   editJob: Job = null;
+  stateAbbrevs: string[] = ['AK', 'AL', 'AR', 'AS', 'AZ', 'CA', 'CO', 'CT',
+                            'DC', 'DE', 'FL', 'GA', 'GU', 'HI', 'IA', 'ID',
+                            'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+                            'MI', 'MN', 'MO', 'MP', 'MS', 'MT', 'NC', 'ND',
+                            'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK',
+                            'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX',
+                            'UM', 'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV',
+                            'WY'];
   constructor(private jobSvc: JobService) { }
 
   ngOnInit(): void {
